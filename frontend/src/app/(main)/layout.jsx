b0d2c4692ea8ea9644621a54557245a32e0bad37
Sarvesh Tiwari <mr.sarveshtiwari@gmail.com>
2025-03-24 13:24:09 +0530

    Navbar

--- frontend/src/app/(main)/layout.jsx
+++ frontend/src/app/(main)/layout.jsx
@@ -1,13 +1,15 @@
-import React from 'react'
-import Navbar from './Navbar';
+import React from "react";
+import Navbar from "../../components/Navbar";
+import Footer from "../../components/Footer";
 
 const Layout = ({ children }) => {
-    return (
-        <>
-            <Navbar />
-            {children}
-        </>
-    )
-}
+  return (
+    <>
+      <Navbar />
+      {children}
+      <Footer />
+    </>
+  );
+};
 
 export default Layout;
---
+++ frontend/src/components/Navbar.jsx
@@ -0,0 +1,129 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <>
+      {/* ========== HEADER ========== */}
+      <header className="flex flex-wrap  md:justify-start md:flex-nowrap z-50 w-full bg-white border-b border-gray-200 dark:bg-neutral-800 dark:border-neutral-700">
+        <nav className="relative max-w-[85rem] w-full mx-auto md:flex md:items-center md:justify-between md:gap-3 py-2 px-4 sm:px-6 lg:px-8">
+          <div className="flex justify-between items-center gap-x-1">
+            <a
+              className="flex-none font-semibold text-xl text-black focus:outline-hidden focus:opacity-80 dark:text-white"
+              href="#"
+              aria-label="Brand"
+            >
+              PodSarveshify{" "}
+            </a>
+            {/* Collapse Button */}
+            <button
+              type="button"
+              className="hs-collapse-toggle md:hidden relative size-9 flex justify-center items-center font-medium text-sm rounded-lg border border-gray-200 text-gray-800 hover:bg-gray-100 focus:outline-hidden focus:bg-gray-100 disabled:opacity-50 disabled:pointer-events-none dark:text-white dark:border-neutral-700 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+              id="hs-header-base-collapse"
+              aria-expanded="false"
+              aria-controls="hs-header-base"
+              aria-label="Toggle navigation"
+              data-hs-collapse="#hs-header-base"
+            >
+              <svg
+                className="hs-collapse-open:hidden size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <line x1={3} x2={21} y1={6} y2={6} />
+                <line x1={3} x2={21} y1={12} y2={12} />
+                <line x1={3} x2={21} y1={18} y2={18} />
+              </svg>
+              <svg
+                className="hs-collapse-open:block shrink-0 hidden size-4"
+                xmlns="http://www.w3.org/2000/svg"
+                width={24}
+                height={24}
+                viewBox="0 0 24 24"
+                fill="none"
+                stroke="currentColor"
+                strokeWidth={2}
+                strokeLinecap="round"
+                strokeLinejoin="round"
+              >
+                <path d="M18 6 6 18" />
+                <path d="m6 6 12 12" />
+              </svg>
+              <span className="sr-only">Toggle navigation</span>
+            </button>
+            {/* End Collapse Button */}
+          </div>
+          {/* Collapse */}
+          <div
+            id="hs-header-base"
+            className="hs-collapse hidden overflow-hidden transition-all duration-300 basis-full grow md:block "
+            aria-labelledby="hs-header-base-collapse"
+          >
+            <div className="overflow-hidden overflow-y-auto max-h-[75vh] [&::-webkit-scrollbar]:w-2 [&::-webkit-scrollbar-thumb]:rounded-full [&::-webkit-scrollbar-track]:bg-gray-100 [&::-webkit-scrollbar-thumb]:bg-gray-300 dark:[&::-webkit-scrollbar-track]:bg-neutral-700 dark:[&::-webkit-scrollbar-thumb]:bg-neutral-500">
+              <div className="py-2 md:py-0  flex flex-col md:flex-row md:items-center gap-0.5 md:gap-1">
+                <div className="grow">
+                  <div className="flex flex-col md:flex-row md:justify-end md:items-center gap-0.5 md:gap-1">
+                    <a
+                      className="p-2 flex items-center text-sm bg-gray-100 text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:bg-neutral-700 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                      href="#"
+                      aria-current="page"
+                    >
+                      Landing
+                    </a>
+                    <a
+                      className="p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                      href="#"
+                    >
+                      Account
+                    </a>
+                    <a
+                      className="p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                      href="/contact"
+                    >
+                      Contact
+                    </a>
+                    <a
+                      className="p-2 flex items-center text-sm text-gray-800 hover:bg-gray-100 rounded-lg focus:outline-hidden focus:bg-gray-100 dark:text-neutral-200 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                      href="/about"
+                    >
+                      About
+                    </a>
+                  </div>
+                </div>
+                <div className="my-2 md:my-0 md:mx-2">
+                  <div className="w-full h-px md:w-px md:h-4 bg-gray-100 md:bg-gray-300 dark:bg-neutral-700" />
+                </div>
+                {/* Button Group */}
+                <div className=" flex flex-wrap items-center gap-x-1.5">
+                  <a
+                    className="py-[7px] px-2.5 inline-flex items-center font-medium text-sm rounded-lg border border-gray-200 bg-white text-gray-800 shadow-2xs hover:bg-gray-50 disabled:opacity-50 disabled:pointer-events-none dark:bg-neutral-800 focus:outline-hidden focus:bg-gray-100 dark:border-neutral-700 dark:text-neutral-300 dark:hover:bg-neutral-700 dark:focus:bg-neutral-700"
+                    href="/login"
+                  >
+                    Sign in
+                  </a>
+                  <a
+                    className="py-2 px-2.5 inline-flex items-center font-medium text-sm rounded-lg bg-blue-600 text-white hover:bg-blue-700 focus:outline-hidden focus:bg-blue-700 disabled:opacity-50 disabled:pointer-events-none dark:bg-blue-500 dark:hover:bg-blue-600 dark:focus:bg-blue-600"
+                    href="/signup"
+                  >
+                    Register
+                  </a>
+                </div>
+                {/* End Button Group */}
+              </div>
+            </div>
+          </div>
+          {/* End Collapse */}
+        </nav>
+      </header>
+      {/* ========== END HEADER ========== */}
+    </>
+  );
+};
+
+export default Navbar;
